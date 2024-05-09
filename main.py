058f8a2d025b2ca973838a121fa4fc171f6526b5
hika138 <hikax138@gmail.com>
2024-05-10 08:12:17 +0900

    最低気温の取得を廃止

--- main.py
+++ main.py
@@ -45,22 +45,22 @@ def get_weather(pref_code: int, area_code: int, local_code: int = 0):
     weather_json = requests.get(weather_url).json()
     weather = ""
     temp={0, 0}
-    # 該当地域の天気を取得
+    # 該当地域の天気予報を取得
     for weather_area in weather_json[0]["timeSeries"][0]["areas"]:
         if int(weather_area["area"]["code"]) == area_code:
                 weather = str(weather_area["weathers"][0]).replace("\u3000", "")
-    # 該当地域の気温を取得
+    # 該当地域の予想最高気温を取得
     for temp_area in weather_json[0]["timeSeries"][2]["areas"]:
         if int(temp_area["area"]["code"]) == local_code:
-                temp = temp_area["temps"]
-    # 該当地域の降水確率を取得
+                temp = temp_area["temps"][0]
+    # 該当地域の日中予想降水確率を取得
     for pops_area in weather_json[0]["timeSeries"][1]["areas"]:
         if int(pops_area["area"]["code"]) == area_code:
-            pops = pops_area["pops"]
+            pops = {pops_area["pops"][0], pops_area["pops"][1], pops_area["pops"][2]}
             pop_sum = 0
             for pop in pops:
                  pop_sum += int(pop)
-        pop_avg = pop_sum / 5   # 30時間分の降水確率の平均値を取得
+        pop_avg = pop_sum / 3   # 30時間分の降水確率の平均値を取得
 
     return weather, temp, int(pop_avg)
 
@@ -69,16 +69,16 @@ async def weather_notify():
     channel = client.get_channel(channel_id)
     tokushima_weather, tokushima_temp, tokushima_pop = get_weather(tokushima_code, tokushima_north_code, tokushima_city_code)
     hyogo_weather, hyogo_temp, hyogo_pop = get_weather(hyogo_code, hyogo_south_code, kobe_city_code)
+    # 天気予報を埋め込みメッセージで通知
     embed = discord.Embed(title="本日の天気予報",
-                      url="https://www.jma.go.jp/bosai/#pattern=forecast",
-                      colour=0x00b0f4,
-                      timestamp=datetime.now())
-
+                    url="https://www.jma.go.jp/bosai/#pattern=forecast",
+                    colour=0x00b0f4,
+                    timestamp=datetime.now())
     embed.add_field(name="徳島",
-                    value=f"{tokushima_weather}\n最高気温: {tokushima_temp[0]} ℃\n最低気温: {tokushima_temp[1]} ℃\n降水確率: {tokushima_pop} %",
+                    value=f"> 天気予報: {tokushima_weather}\n> 最高気温: {tokushima_temp} ℃\n> 降水確率: {tokushima_pop} %",
                     inline=True)
     embed.add_field(name="兵庫",
-                    value=f"{hyogo_weather}\n最高気温: {hyogo_temp[0]} ℃\n最低気温: {hyogo_temp[1]} ℃\n降水確率: {hyogo_pop} %",
+                    value=f"> 天気予報: {hyogo_weather}\n> 最高気温: {hyogo_temp} ℃\n> 降水確率: {hyogo_pop} %",
                     inline=True)
     await channel.send(embed=embed)
 
